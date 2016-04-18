7b46d30ba8fcd11a9e59cb04ce50fd3ca9a74c9e
Roy Jung <phi4grv@gmail.com>
2016-04-19 01:26:01 +1000

    Add tests for settable options

--- spec/omniauth/strategies/parti_spec.rb
+++ spec/omniauth/strategies/parti_spec.rb
@@ -4,10 +4,10 @@ describe OmniAuth::Strategies::Parti do
       [200, {}, ["Hello."]]
     end
   end
-  let(:strategy_class) { Class.new(OmniAuth::Strategies::Parti) }
+  let(:strategy_class) { Class.new OmniAuth::Strategies::Parti }
 
   describe 'default options' do
-    subject { strategy_class.new(app) }
+    subject { strategy_class.new app }
     it 'has correct issuer' do
       expect(subject.options.issuer).to eq('https://v1.api.parti.xyz')
     end
@@ -42,6 +42,28 @@ describe OmniAuth::Strategies::Parti do
 
     it 'has empty client secret' do
       expect(subject.options.client_options.secret).to eq(nil)
+    end
+  end
+
+  describe 'settable options' do
+    it 'sets issuer' do
+      subject = strategy_class.new app, issuer: 'http://another-issuer.com'
+      expect(subject.options.issuer).to eq('http://another-issuer.com')
+    end
+
+    it 'sets redirect uri' do
+      subject = strategy_class.new app, client_options: { redirect_uri: 'http://redirect-uri.com' }
+      expect(subject.options.client_options.redirect_uri).to eq('http://redirect-uri.com')
+    end
+
+    it 'sets client id' do
+      subject = strategy_class.new app, client_options: { identifier: 'client-identifer' }
+      expect(subject.options.client_options.identifier).to eq('client-identifer')
+    end
+
+    it 'sets client secret' do
+      subject = strategy_class.new app, client_options: { secret: 'client-secret' }
+      expect(subject.options.client_options.secret).to eq('client-secret')
     end
   end
 end
